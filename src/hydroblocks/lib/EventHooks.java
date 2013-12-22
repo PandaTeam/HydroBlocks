2c6546492dee4bb1d1f05bad10b55ef9d318849d
yksrep <yksrep@gmail.com>
2013-12-22 20:05:37 +1000

    Updated EventHooks

--- src/hydroblocks/lib/EventHooks.java
+++ src/hydroblocks/lib/EventHooks.java
@@ -1,51 +1,52 @@
 package hydroblocks.lib;
 
-import hydroblocks.items.Items;
-
 import java.util.Random;
 
+import hydroblocks.items.Items;
 import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.ForgeSubscribe;
-import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
-import net.minecraftforge.oredict.OreDictionary;
-
-
+import net.minecraftforge.event.world.BlockEvent;
 
 public class EventHooks {
-		Random random = new Random();
+	Random random = new Random();
 
-		@ForgeSubscribe
-		public void onHarvestDrops(HarvestDropsEvent event) {
-		if (event.harvester != null && event.harvester.getHeldItem() != null && event.harvester.getHeldItem().itemID == Items.ironsledgehammer.itemID 
-		|| event.harvester.getHeldItem().itemID == Items.goldsledgehammer.itemID || event.harvester.getHeldItem().itemID == Items.diamondsledgehammer.itemID
-		|| event.harvester.getHeldItem().itemID == Items.cobblesledgehammer.itemID) {
+
+@ForgeSubscribe
+public void onHarvestDrops(BlockEvent.HarvestDropsEvent event)
+
+{
+
+EntityPlayer player = event.harvester;
+ItemStack heldItem = player.inventory.getCurrentItem();
+Block block = event.block;
+
+if(heldItem.itemID == Items.cobblesledgehammer.itemID || heldItem.itemID == Items.ironsledgehammer.itemID
+|| heldItem.itemID == Items.goldsledgehammer.itemID || heldItem.itemID == Items.diamondsledgehammer.itemID)
+
+{
+
+	if (event.block.blockID == Block.oreIron.blockID) {
 		event.drops.clear();
-		event.dropChance = 1.0F;
-
-		if (event.block.blockID == Block.oreIron.blockID) {
-		event.drops.add(new ItemStack(Items.ironfragments, random.nextInt(2) + 1));
-		}
-		if (event.block.blockID == Block.oreGold.blockID) {
-		event.drops.add(new ItemStack(Items.goldfragments, random.nextInt(2) + 1));
-		}
-		if (event.block.blockID == OreDictionary.getOreID("oreTin")) {
-		event.drops.add(new ItemStack(Items.tinfragments, random.nextInt(2) + 1));
-		}
-		if (event.block.blockID == OreDictionary.getOreID("oreCopper")) {
-		event.drops.add(new ItemStack(Items.copperfragments, random.nextInt(2) + 1));
-		}
-		}
-		}
+        event.drops.add(new ItemStack(Items.ironfragments, random.nextInt(2) + 1));
+        event.dropChance = 1.0F;
+        }
+        if (event.block.blockID == Block.oreGold.blockID) {
+    	event.drops.clear();
+        event.drops.add(new ItemStack(Items.goldfragments, random.nextInt(2) + 1));
+        event.dropChance = 1.0F;
+        }
+       // if (event.block.blockID == OreDictionary.getOreID("oreTin")) {
+		//event.drops.clear();
+       // event.drops.add(new ItemStack(Items.tinfragments, random.nextInt(2) + 1));
+        //event.dropChance = 1.0F;
+      //  }
+        //if (event.block.blockID == OreDictionary.getOreID("oreCopper")) {
+		//event.drops.clear();
+       // event.drops.add(new ItemStack(Items.copperfragments, random.nextInt(2) + 1));
+        //event.dropChance = 1.0F;
+       // }
+        }
+        }
 }
-
-
-
-		
-
- 
-
-                        		
-                        
-                                        
-                
