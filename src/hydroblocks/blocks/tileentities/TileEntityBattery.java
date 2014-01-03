b492bcdf27bb34ed5deaa36b9ba4e92e1b811338
yksrep <yksrep@gmail.com>
2014-01-03 17:11:48 +1000

    Update

--- src/hydroblocks/blocks/tileentities/TileEntityBattery.java
+++ src/hydroblocks/blocks/tileentities/TileEntityBattery.java
@@ -13,14 +13,7 @@ import universalelectricity.compatibility.TileEntityUniversalElectrical;
 import universalelectricity.core.block.IElectrical;
 
 public class TileEntityBattery extends TileEntityUniversalElectrical implements IElectrical{
-	
-	
 
-
-	
-	
-	
-	
 	
 	@Override
 	public EnumSet<ForgeDirection> getOutputDirections()
@@ -33,19 +26,22 @@ public class TileEntityBattery extends TileEntityUniversalElectrical implements 
 		 return (float) Math.min((this.getMaxEnergyStored() - this.getEnergyStored()), 200);
 	}
 
+	
+	// three different outputs 100, 1000, 10000
 	@Override
 	public float getProvide(ForgeDirection direction) {
-		if (energyStored >= 1){
-			return 1;
+		if (energyStored >= 100){
+			return 100;
 		} else
-		return 0;
+		return energyStored;
 		
 	}
-
+// three different power storage levels 50000, 500000, 5000000.
+	
+	int MaxEnergyStored = 50000;
 	@Override
 	public float getMaxEnergyStored() {
-		// TODO Auto-generated method stub
-		return 10000;
+		return MaxEnergyStored;
 	}
 	
 	@Override
@@ -58,49 +54,49 @@ public class TileEntityBattery extends TileEntityUniversalElectrical implements 
 		
 		
 		//1 to 10 %
-		if (energyStored >= 0.00 && energyStored <= 1000.00) {
+		if (energyStored >= 0.00 && energyStored <= 5000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 0, 3);
 			
 		}
 		//10 % to 20%
-		if (energyStored >= 1000.00 && energyStored <= 1999.00) {
+		if (energyStored >= 5000.00 && energyStored <= 10000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 1, 3);
 		}
 		//20% - 30%
-		if (energyStored >= 2000.00 && energyStored <= 2999.00 ) {
+		if (energyStored >= 10000.00 && energyStored <= 15000.00 ) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 2, 3);
 		}
 		//30% - 40%
-		if (energyStored >= 3000.00 && energyStored <= 3999.00) {
+		if (energyStored >= 15000.00 && energyStored <= 20000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 3, 3);
 		}
 		//40% - 50%
-		if (energyStored >= 4000.00 && energyStored <= 4999.00) {
+		if (energyStored >= 20000.00 && energyStored <= 25000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 4, 3);
 		}
 		// 50% - 60%
-		if (energyStored >= 5000.00 && energyStored <= 5999.00) {
+		if (energyStored >= 25000.00 && energyStored <= 30000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 5, 3);
 		}
 		//60% - 70%
-		if (energyStored >= 6000.00 && energyStored <= 6999.00) {
+		if (energyStored >= 30000.00 && energyStored <= 35000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 6, 3);
 		}
 		//70% - 80%
-		if (energyStored >= 7000.00 && energyStored <= 7999.00) {
+		if (energyStored >= 35000.00 && energyStored <= 40000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 7, 3);
 		}
 		//80% - 90%
-		if (energyStored >= 8000.00 && energyStored <= 8999.00) {
+		if (energyStored >= 40000.00 && energyStored <= 45000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 8, 3);
 		}
 		//90 % - 100%
-		if (energyStored >= 9000.00 && energyStored <= 9999.00) {
+		if (energyStored >= 45000.00 && energyStored <= 49999.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 9, 3);
 		}
 		
 		//100%
-		if (energyStored > 9999.00) {
+		if (energyStored > 50000.00) {
 			worldObj.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 10, 3);
 		}
 				
@@ -129,7 +125,6 @@ public class TileEntityBattery extends TileEntityUniversalElectrical implements 
     public Packet getDescriptionPacket() {
         NBTTagCompound nbtTag = new NBTTagCompound();
         this.writeToNBT(nbtTag);
-//this packet already handles the problem you have.
         return new Packet132TileEntityData(this.xCoord, this.yCoord, this.zCoord, 1, nbtTag);
 }
 
